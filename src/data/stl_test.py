a916ad23f25e8c1bf66f6161b84c0bcbf2801f5e
AndersManneraak <andersmannerak@hotmail.com>
2024-03-09 22:29:30 +0100

    drg

--- src/data/stl_test.py
+++ src/data/stl_test.py
@@ -4,8 +4,10 @@ from sql_database import SQLDatabase
 db = SQLDatabase()
 df = db.fetch_query_result("select * from stage_financials.st_balance_sheet_y where ticker = 'GME'")
 reset_df = df.set_index(['calendar_year'])
-trans_df = reset_df.transpose()
+sorted_df = reset_df.sort_index(axis=0, ascending=False, inplace=False, kind="quicksort")
+trans_df = sorted_df.transpose()
 reversed_df = trans_df[reversed(trans_df.columns)]
+print(reversed_df)
 
 with st.sidebar:
     st.dataframe(reversed_df, use_container_width=True)
